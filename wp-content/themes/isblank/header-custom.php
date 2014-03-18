6323cb483486933445068ab9fd2fb545293b6b53
admin <lcdvirgo@gmail.com>
2014-03-18 03:15:10 -0700

    style

--- wp-content/themes/isblank/header-custom.php
+++ wp-content/themes/isblank/header-custom.php
@@ -44,7 +44,7 @@ $(document).ready( function() {
     	$('#typedcomplete').delay(16000).fadeOut();
 	$("#tagline span").delay(17500).text("What's yours?").fadeIn();
 	$("#button span").delay(19500).text("Find out now").fadeIn();
-	$("#menu").css("display", "none !important");
+	$("#menu").css("display", "none");
       });
 	</script>
 
